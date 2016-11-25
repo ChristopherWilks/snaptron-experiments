d1d41f11d74eb63069e4637b3a1749852bd6cfc3
Chris Wilks <broadsword@gmail.com>
2016-11-25 15:34:03 -0500

    added R scripts for TS and SSC

---
+++ scripts/validation_boxplot.R
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+infile <- file("stdin")
+m<-read.delim(infile)
+mx<-max(subset(m,group == 'non_validated', select=c(shared_sample_counts)))
+m$group <- factor(m$group,labels=c('Did not validate', 'Validated'))
+
+pdf("./shared_sample_counts.pdf")
+boxplot(m$shared_sample_counts ~ m$group, ylab="Shared-sample count")
+tg <- wilcox.test(m$shared_sample_counts ~ m$group, alternative="less")
+tgp <- paste(c("Wilcox pval",round(tg$p.value,digits=6)), collapse=" ")
+text(1.45, mx+10, tgp)
+dev.off()
