40ead06e11d3f99766433f0f5c606907ec0d4a50
Chris Wilks <broadsword@gmail.com>
2016-11-25 16:11:57 -0500

    added clarification comment on inclusion of bed2snaptron.py

--- scripts/run_ssc.sh
+++ scripts/run_ssc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#included for this analysis: an example of converrting BED formatted *exons* to Snaptron formatted junction finding input file
 cat data/novel_exons.raw.hg38.bed | python scripts/bed2snaptron.py > ./data/novel_exons.hg38.snap.tsv
 
+#run the actual analysis using the SRAv2 data source
 cd client/
 python query_snaptron.py --query-file ../data/novel_exons.hg38.snap.tsv --function shared --datasrc srav2 > ../data/novel_exons.hg38.ssc_results.tsv
