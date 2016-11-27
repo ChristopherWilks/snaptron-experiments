b6f9bb7bf2dcd2aada9561fbce9f0bb4e5c8cf00
Chris Wilks <broadsword@gmail.com>
2016-11-27 00:03:06 -0500

    run_ssc.sh now working

--- scripts/run_ssc.sh
+++ scripts/run_ssc.sh
@@ -5,4 +5,7 @@ cat data/novel_exons.raw.hg38.bed | python scripts/bed2snaptron.py > ./data/nove
 
 #run the actual analysis using the SRAv2 data source
 cd client/
-python query_snaptron.py --query-file ../data/novel_exons.hg38.snap.tsv --function shared --datasrc srav2 | perl -ne '$s=$_; if(!$p) { print "group\tvalidated\tshared_sample_counts\n"; next; $p=1;} $s=~s/ /\t/;' > ../data/novel_exons.hg38.ssc_results.tsv
+python query_snaptron.py --query-file ../data/novel_exons.hg38.snap.tsv --function shared --datasrc srav2 | perl -ne '$s=$_; if(!$p) { print "group\tvalidation\tshared_sample_counts\n"; $p=1; next} $s=~s/ /\t/; print "$s";'  > ../data/novel_exons.hg38.ssc_results.tsv
+
+cd ../
+cat ./data/novel_exons.hg38.ssc_results.tsv | Rscript ./scripts/validation_boxplot.R
