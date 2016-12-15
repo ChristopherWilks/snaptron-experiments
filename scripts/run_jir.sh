27e51cada2f5cf759db35dd753e20b2dd247e7e0
Chris Wilks <broadsword@gmail.com>
2016-12-15 18:53:34 -0500

    renamed to use the .snap. suffix consistently

--- scripts/run_jir.sh
+++ scripts/run_jir.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 cd client/
-python query_snaptron.py --query-file ../data/alk_alt_tss.hg19.tsv --function jir --datasrc srav1 > ../data/alk_alt_tss.hg19.jir_results.tsv
+python query_snaptron.py --query-file ../data/alk_alt_tss.hg19.snap.tsv --function jir --datasrc srav1 > ../data/alk_alt_tss.hg19.jir_results.tsv
+
+python query_snaptron.py --query-file ../data/alk_alt_tss.hg38.snap.tsv --function jir --datasrc tcga > ../data/alk_alt_tss.hg38.jir_results.tsv
+
