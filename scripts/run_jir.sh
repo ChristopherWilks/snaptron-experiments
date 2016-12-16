2222a93da01fa6c6b69121b4bee9f8b303caa230
Chris Wilks <broadsword@gmail.com>
2016-12-15 19:56:10 -0500

    added GTEx/TCGA to run_jir.sh

--- scripts/run_jir.sh
+++ scripts/run_jir.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 cd client/
-python query_snaptron.py --query-file ../data/alk_alt_tss.hg19.snap.tsv --function jir --datasrc srav1 > ../data/alk_alt_tss.hg19.jir_results.tsv
+#SRAv1 (gives results consist with paper)
+python query_snaptron.py --query-file ../data/alk_alt_tss.hg19.snap.tsv --function jir --datasrc srav1 > ../data/alk_alt_tss.hg19.srav1.jir_results.tsv
 
-python query_snaptron.py --query-file ../data/alk_alt_tss.hg38.snap.tsv --function jir --datasrc tcga > ../data/alk_alt_tss.hg38.jir_results.tsv
+#GTEx
+python query_snaptron.py --query-file ../data/alk_alt_tss.hg38.snap.tsv --function jir --datasrc gtex > ../data/alk_alt_tss.hg38.gtex.jir_results.tsv
 
+#TCGA
+python query_snaptron.py --query-file ../data/alk_alt_tss.hg38.tcga.snap.tsv --function jir --datasrc tcga > ../data/alk_alt_tss.hg38.tcga.jir_results.tsv
