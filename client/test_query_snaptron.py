6e81ec6010dcf401b9d5dc28d70eea08e3527f88
Chris Wilks <broadsword@gmail.com>
2016-12-17 19:11:14 -0500

    fixed header diff bug

--- client/test_query_snaptron.py
+++ client/test_query_snaptron.py
@@ -11,7 +11,7 @@ args = None
 def setUpModule():
     global sample_records
     global args
-    parser = query_snaptron.create_parser(disable_header=True)
+    parser = query_snaptron.create_parser(disable_header=False)
     args = parser.parse_args()
     sample_records = query_snaptron.download_sample_metadata(args)
     pass
@@ -32,7 +32,6 @@ class TestFunctions(unittest.TestCase):
         expected_output=["jir_score\tA raw count\tB raw count\t%s\n" % sample_records["header"]]
         #sample_records[sample_1]="filler"
         #sample_records[sample_2]="filler"
-        expected_output = []
         expected_output.append("0.5\t1\t2\t%s\n" % (sample_records[sample_1]))
         expected_output.append("0.333333333333\t2\t3\t%s\n" % (sample_records[sample_2]))
         output = query_snaptron.junction_inclusion_ratio(args, {'samples':{sample_1:{"A":1,"B":2},sample_2:{"A":2,"B":3}}}, ["A","B"], sample_records)
