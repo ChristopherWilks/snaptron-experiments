b8acd4af3024df1334bbc12eeb93bde345864e41
BenLangmead <langmea@cs.jhu.edu>
2016-12-22 11:18:20 -0500

    improvements to shared sample count plot

--- scripts/validation_boxplot.R
+++ scripts/validation_boxplot.R
@@ -1,12 +1,21 @@
 #!/usr/bin/env Rscript
-infile <- file("stdin")
-m<-read.delim(infile)
-mx<-max(subset(m,validation == 'validated', select=c(shared_sample_counts)))
-m$group <- factor(m$validation,labels=c('Did not validate', 'Validated'))
 
-pdf("./shared_sample_counts.pdf")
-boxplot(m$shared_sample_counts ~ m$group, ylab="Shared-sample count")
-tg <- wilcox.test(m$shared_sample_counts ~ m$group, alternative="less")
-tgp <- paste(c("Wilcox pval",round(tg$p.value,digits=6)), collapse=" ")
-text(1.45, mx+10, tgp)
+library('ggplot2')
+
+# Read tables
+args = commandArgs(trailingOnly=TRUE)
+m_s <- read.table(args[1], sep='\t', quote='', comment.char='', header=T)
+m_g <- read.table(args[2], sep='\t', quote='', comment.char='', header=T)
+m_s$dataset <- 'SRAv2'
+m_g$dataset <- 'GTEx'
+m <- rbind(m_s, m_g)
+
+# Plot
+pdf(file='shared_sample_counts.pdf', width=4, height=4)
+ggplot(m, aes(factor(dataset), shared_sample_counts)) +
+	  geom_boxplot(aes(fill=factor(validation, labels=c('Failed', 'Passed')))) +
+	  theme_bw() +  # less gray
+	  labs(x='\nData compilation', y='Shared sample count (SSC)\n') +  # change axis labels
+	  scale_fill_discrete(name="Validation") +  # change legend label
+	  theme(legend.justification=c(0,1), legend.position=c(0,1))  # put legend in upper left
 dev.off()
