e6c4d6b61592235edb261de6b83c0764dbcd9c0e
Chris Wilks <broadsword@gmail.com>
2017-01-05 23:27:22 -0500

    added compressed/uncompressed tabix

--- performance_plots/benchmark_fig.R
+++ performance_plots/benchmark_fig.R
@@ -3,10 +3,12 @@
 library('ggplot2')
 
 m <- read.table('benchmark_25_10_snaptron_only.tsv', header=T, sep='\t')
-m$Tabix <- grepl('Tabix', m$Query.Type)
+m$TabixC <- grepl('Tabix-Compressed', m$Query.Type)
+m$TabixU <- grepl('Tabix-Uncompressed', m$Query.Type)
 m$Constrained <- factor(grepl('Constraint', m$Query.Type), labels=c('R query', 'R+F query'))
 m$NoIndex <- grepl('NoIndex', m$Query.Type)
-m$Type <- ifelse(m$Tabix, 'Tabix, R-tree', ifelse(m$NoIndex, 'SQLite, no index', 'SQLite, B-tree'))
+#m$Type <- ifelse(m$TabixC, 'Tabix, R-tree, compressed', ifelse(m$TabixU, 'Tabix, R-tree, uncompressed', ifelse(m$NoIndex, 'SQLite, no index, uncompressed', 'SQLite, B-tree, uncompressed')))
+m$Type <- ifelse(m$TabixC, 'Tabix, R-tree, compressed', ifelse(m$TabixU, 'Tabix, R-tree', ifelse(m$NoIndex, 'SQLite, no index', 'SQLite, B-tree')))
 
 pdf(file="benchmark_fig.pdf", height=4, width=5.5)
 ggplot(m, aes(x=Query.Size, y=Query.Time, color=Type, linetype=Constrained)) + labs(x='Query size (100KB)', y='Time (seconds)') + geom_line() + geom_point() + theme_bw() + theme(legend.title=element_blank()) + scale_y_continuous(limits=c(0, 17))
