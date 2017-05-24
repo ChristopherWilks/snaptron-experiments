7c84b76197559ebb64410c089ab03169ad8361e3
Chris Wilks <broadsword@gmail.com>
2017-05-24 02:39:28 -0400

    setup script to pull down SciSCript-Python; basic SciServer Snaptron2 test script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/ChristopherWilks/SciScript-Python.git ./SciScript-Python
+ln -s SciScript-Python/py2/SciServer ./SciServer
+
---
+++ test_ss.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+
+
+import SciServer
+from SciServer import Authentication, LoginPortal, Config, CasJobs, SkyQuery, SciDrive, SkyServer
+import os;
+import sys;
+import json;
+
+Authentication_loginName = 'snaptron2_agent';
+Authentication_loginPassword = ''
+token1 = Authentication.login(Authentication_loginName, Authentication_loginPassword);
+
+CasJobs_TestDatabase = "Snaptron2"
+CasJobs_TestQuery = "SELECT top 10 * from SnapCounts"
+CasJobs_TestCSVFile = "SciScriptTestFile.csv"
+
+#tables = CasJobs.getTables(context="Snaptron2")
+#print tables
+df = CasJobs.executeQuery(sql=CasJobs_TestQuery, context=CasJobs_TestDatabase, format="csv")
+print df
