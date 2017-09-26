66afa166e685e18d829b53c0ed0613708f04e9e1
Chris Wilks <broadsword@gmail.com>
2017-09-26 16:37:30 -0400

    adding JIR based test files and test method

--- client/test_query_snaptron.py
+++ client/test_query_snaptron.py
@@ -23,33 +23,52 @@ class TestSampleCountingPerGroup(unittest.TestCase):
     def setUp(self):
         pass
    
+    def general_test_first_and_full_processing(self, query_id, raw_file, args, group, results, first_results, full_results):
+        with open(raw_file, 'r') as fin:
+            query_snaptron.count_samples_per_group(args, results, fin.readline().rstrip(), group, out_fh=None)
+            query_snaptron.count_samples_per_group(args, results, fin.readline().rstrip(), group, out_fh=None)
+           
+            #first record processed (after header) test
+            self.assertEqual(first_results, results) 
+        
+            #keep going to the end of the file
+            for line in fin:
+                query_snaptron.count_samples_per_group(args, results, line.rstrip(), group, out_fh=None)
+
+            #now check the full results data structure
+            self.assertEqual(full_results, results) 
+
+    def test_jir_count_samples_per_group(self):
+        args = Namespace(function='jir')
+        results = {'samples':{},'queries':[],'exons':{'start':{},'end':{}},'either':0}
+
+        for (idx,group) in enumerate(list(['A_NormalTSS','B_AltTSS'])):
+            first_results = test_data.jir_results_first_time_through[group]
+            full_results = test_data.jir_results_after_first_basic_query[group]
+            raw_file = '../tests/jir.raw%d' % (int(idx)+1)
+            self.general_test_first_and_full_processing(group, raw_file, args, group, results, first_results, full_results)
+            
+
     #SSC uses the core parts of the count_samples_per_group(...) method
     def test_ssc_count_samples_per_group(self):
-        group = 'C1orf100 non_validated'
         args = Namespace(function='shared')
         
+        group = 'C1orf100 non_validated'
+
+        results = {'samples':{},'queries':[],'exons':{'start':{},'end':{}}}
+        results['groups_seen']={group:1}
+        results['shared']={}
+        results['annotated']={group:{}}
+        results['annotated'][group][results['groups_seen'][group]]=0
+        results['annotations']={group:{}}
+        
         #2 tests per modifier value (1 or 2)
-        for either in (1,2):
-            results = {'samples':{},'queries':[],'exons':{'start':{},'end':{}},'either':either}
-            results['groups_seen']={group:1}
-            results['shared']={}
-            results['annotated']={group:{}}
-            results['annotated'][group][results['groups_seen'][group]]=0
-            results['annotations']={group:{}}
-
-            with open('../tests/C1orf100.raw%d' % either,'r') as fin:
-                query_snaptron.count_samples_per_group(args, results, fin.readline().rstrip(), group, out_fh=None)
-                query_snaptron.count_samples_per_group(args, results, fin.readline().rstrip(), group, out_fh=None)
-               
-                #first record processed (after header) test
-                self.assertEqual(test_data.C1orf100_results_first_time_through[either], results) 
-            
-                #keep going to the end of the file
-                for line in fin:
-                    query_snaptron.count_samples_per_group(args, results, line.rstrip(), group, out_fh=None)
-
-                #now check the full results data structure
-                self.assertEqual(test_data.C1orf100_results_after_first_basic_query[either], results) 
+        for either in (2,1):
+            results['either'] = either
+            first_results = test_data.C1orf100_results_first_time_through[either]
+            full_results = test_data.C1orf100_results_after_first_basic_query[either]
+            raw_file = '../tests/C1orf100.raw%d' % either
+            self.general_test_first_and_full_processing(either, raw_file, args, group, results, first_results, full_results)
 
 
 class TestJIR(unittest.TestCase):
