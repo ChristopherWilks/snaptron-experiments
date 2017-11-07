916a94bef32a7504f4a829b3c3968117b4da20df
Chris Wilks <broadsword@gmail.com>
2017-11-07 14:19:54 -0500

    added norm. example

--- examples.sh
+++ examples.sh
@@ -5,13 +5,13 @@ python client/query_snaptron.py --region "chr2:29446395-30142858" --contains 1 -
 python client/query_snaptron.py --region "chr11:82970135-82997450" --contains 1 --filters "samples_count>=7&coverage_sum>=10" --metadata "description:cortex" | wc -l
 python client/query_snaptron.py --region "chr2:29446395-30142858" --contains 1 --function exon --exon-length 57-57 | wc -l
 python client/query_snaptron.py --query-file data/small_ssc_test.snap.tsv --function shared --datasrc gtex
-python client/query_snaptron.py --metadata "description:cortex" >cortex_samples.tsv
-python client/query_snaptron.py --query-file data/sox9_retained.hg38.tcga.snap.tsv --datasrc tcga --function jir > sox9_jir_samples.tsv
-python client/query_snaptron.py --query-file data/test_psi_abcd3.snap.tsv --function psi
+python client/query_snaptron.py --metadata "description:cortex" | wc -l
+python client/query_snaptron.py --query-file data/sox9_retained.hg38.tcga.snap.tsv --datasrc tcga --function jir | wc -l
+python client/query_snaptron.py --query-file data/test_psi_abcd3.snap.tsv --function psi | wc -l
 
-python client/query_snaptron.py --bulk-query-file data/rel_splices.hg38.snap.tsv
+python client/query_snaptron.py --bulk-query-file data/rel_splices.hg38.snap.tsv | wc -l
 
-python client/query_snaptron.py --func intersection --query-file data/test_intersection.snap.tsv > intersect1
-python client/query_snaptron.py --func intersection --query-file data/test_intersection2.snap.tsv > intersect2
+python client/query_snaptron.py --func intersection --query-file data/test_intersection.snap.tsv | wc -l
+python client/query_snaptron.py --func intersection --query-file data/test_intersection2.snap.tsv | wc -l
 
 python client/query_snaptron.py --region "chr2:29446395-30142858" --contains 1 --filters "samples_count>=100&annotated=1" --endpoint genes --normalize recount | wc -l
