b93745a1854707a5c10ba1e479378c0db3d89d84
Chris Wilks <broadsword@gmail.com>
2018-06-14 18:18:24 -0400

    updated main ts script to use KW adjustment script

--- scripts/run_ts.sh
+++ scripts/run_ts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd client/
-python query_snaptron.py --query-file ../data/rel_splices.hg38.snap.tsv --function ts --datasrc gtex | tee ../rel_ts_list.tsv | Rscript ../scripts/tissue_specificty_testing.R
+python query_snaptron.py --query-file ../data/rel_splices.hg38.snap.tsv --function ts --datasrc gtex | tee ../rel_ts_list.tsv | ../scripts/adjust_kruskal_wallis_input.sh | tee ../rel_ts_list.tsv.adjusted | Rscript ../scripts/tissue_specificty_testing.R
